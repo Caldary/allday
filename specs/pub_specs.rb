cc8ce827fee3d30340904739d9d9b16f7296e363
Paulo Rocha <paulorocha1988@gmail.com>
2020-04-08 13:24:27 +0100

    finished mvp again

--- specs/pub_specs.rb
+++ specs/pub_specs.rb
@@ -2,11 +2,20 @@ require('minitest/autorun')
 require('minitest/reporters')
 MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
 require_relative('../pub')
+require_relative('../customer')
+require_relative('../drink')
 
 class PubTest < MiniTest::Test
 
     def setup()
         @pub = Pub.new("The Red Lion", 1000)
+
+        @drink3 = Drink.new("Vodka", 30)
+        @drink4 = Drink.new("White Wine", 15)
+
+        @customer3 = Customer.new("Katie", 200)
+        @customer4 = Customer.new("Hannah", 30)
+
     end
 
     def test_pub_name()
@@ -15,6 +24,11 @@ class PubTest < MiniTest::Test
 
     def test_till_amount()
         assert_equal(1000, @pub.till())
+    end
+
+    def test_sold_a_drink()
+        @pub.sale(@drink3.price)
+        assert_equal(1030, @pub.till())
     end
 
 end
