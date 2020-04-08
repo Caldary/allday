324a3ccee888bd5997aa2ae6d97d59b3922167ca
Paulo Rocha <paulorocha1988@gmail.com>
2020-04-08 11:14:51 +0100

    drink test 1 pass

---
+++ specs/drink_specs.rb
@@ -0,0 +1,18 @@
+require('minitest/autorun')
+require('minitest/reporters')
+MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
+require_relative('../drink')
+
+class DrinkTest < MiniTest::Test
+
+    def setup()
+        @drink1 = Drink.new("Whisky", 20)
+        @drink2 = Drink.new("Red Wine", 10)
+    end
+
+    def test_drink_name()
+        assert_equal("Whisky", @drink1.name)
+    end
+
+end
+
