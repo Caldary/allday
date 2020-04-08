19aab677384db475c931c2b5ee720a1bd8db0be2
Paulo Rocha <paulorocha1988@gmail.com>
2020-04-08 11:19:53 +0100

    all 6 files added

---
+++ specs/customer_specs.rb
@@ -0,0 +1,17 @@
+require('minitest/autorun')
+require('minitest/reporters')
+MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
+require_relative('../customer')
+
+class CustomerTest < MiniTest::Test
+
+    def setup()
+        @customer1 = Customer.new("Eugene", 100)
+        @customer2 = Customer.new("Colin", 10)
+    end
+
+    def test_customer_name()
+        assert_equal("Eugene", @customer1.name)
+    end
+
+end
